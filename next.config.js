153570215577b0134f6aadbe40833372284d0405
peter anyaogu <anyaogupeter601@gmail.com>
2022-03-10 13:16:04 +0100

    fixed known bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextTranslate = require("next-translate");
 
-module.exports = nextConfig
+module.exports = nextTranslate({
+    future: {
+        strictPostcssConfiguration: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    reactStrictMode: true,
+    webpack: (config, { dev, isServer }) => {
+        if (isServer) {
+            // require("./scripts/generate-sitemap");
+        }
+        // required for webpack configurations
+        return config;
+    },
+});
+
+// the security headers should be considered
