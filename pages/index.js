b79da962b1e14eb2c20217a2ee731e40ec9eabae
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-03-31 15:19:00 +0100

    Setup ApolloClient

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,28 @@ import styles from "../styles/Home.module.css";
 import Channel from "../components/Landing/channel";
 import Footer from "../components/Footer";
 import { Box } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient,HttpLink, InMemoryCache } from '@apollo/client';
+
+// Replace with our near-testnet/graphql
+const tetsnet = 'https://mintbase-testnet.hasura.app/v1/graphql'
+///const mainnet = https://mintbase-mainnet.hasura.app/v1/graphql
+    const client = new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: tetsnet,
+      credentials: 'same-origin',
+      headers: {
+        'x-hasura-role': 'anonymous',
+      },
+    }),
+    cache: new InMemoryCache(),
+  })
+
+
 
 export default function Home() {
     return (
+        <ApolloProvider client={client}>
         <Layout>
             <Box className="flex flex-col">
                 <HeroSection />
@@ -16,5 +35,6 @@ export default function Home() {
                 <Footer />
             </Box>
         </Layout>
+        </ApolloProvider>
     );
 }
