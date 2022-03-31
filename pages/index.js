222f2e9463738d030b71c0e9ab9f250b62327856
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-03-31 17:03:03 +0100

    Graphql build

--- pages/index.js
+++ pages/index.js
@@ -4,37 +4,58 @@ import styles from "../styles/Home.module.css";
 import Channel from "../components/Landing/channel";
 import Footer from "../components/Footer";
 import { Box } from "@chakra-ui/react";
-import { ApolloProvider, ApolloClient,HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-// Replace with our near-testnet/graphql
-const tetsnet = 'https://mintbase-testnet.hasura.app/v1/graphql'
-///const mainnet = https://mintbase-mainnet.hasura.app/v1/graphql
-    const client = new ApolloClient({
-    ssrMode: typeof window === 'undefined',
-    link: new HttpLink({
-      uri: tetsnet,
-      credentials: 'same-origin',
-      headers: {
-        'x-hasura-role': 'anonymous',
-      },
-    }),
-    cache: new InMemoryCache(),
-  })
-
-
-
-export default function Home() {
-    return (
-        <ApolloProvider client={client}>
-        <Layout>
-            <Box className="flex flex-col">
-                <HeroSection />
-                <Features />
-                <Team />
-                <Channel />
-                <Footer />
-            </Box>
-        </Layout>
-        </ApolloProvider>
-    );
+export default function Home({ nftDetails }) {
+  console.log("nftDetails", nftDetails)
+  return (
+    <Layout>
+      <Box className="flex flex-col">
+        <HeroSection />
+        <Features />
+        <Team />
+        <Channel />
+        <Footer />
+      </Box>
+    </Layout>
+  );
 }
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    // Replace with our near-testnet/graphql
+    uri: 'https://api.path_to_aerx_near-testnet/graphql/',
+    cache: new InMemoryCache()
+  });
+
+  // This is an example query, we need the actual data struct stored on the NEAR testnet
+  // to build the query
+  const { data } = await client.query({
+    query: gql`
+    
+      query GetNFTs {
+        mintedNFTs(limit: 10) {
+          id
+          name
+          owner
+          minter
+          date
+          claimed {
+            id
+            name
+          }
+        }
+      }
+    `
+  });
+
+  console.log("data", data);
+
+  //With this we retrieve the details of the NFT and can display it to the user
+  return {
+    props: {
+      nftDetails: data.mintedNFTs
+    }
+  }
+}
+
