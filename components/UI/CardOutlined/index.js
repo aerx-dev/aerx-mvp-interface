53c22ae4f41c8f0afb6a5fc19acf90750b9a0ded
Reshma <reshmashaik3011@gmail.com>
2022-05-17 17:35:18 +0530

    Deleted unwanted components

--- components/UI/CardOutlined/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-
-const CardOutlined = (props) => {
-    return <div className={styles.outlinedCard}>{props.children}</div>;
-};
-
-export default CardOutlined;
--- components/UI/Icons/index.js
+++
@@ -1,2 +0,0 @@
-import { Icon } from "@chakra-ui/react";
-
