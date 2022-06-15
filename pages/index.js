3d3907297ac7a3e2f4d6a4886e6d3d3da7ccba9b
diposs <onakunle.dipo@gmail.com>
2022-06-16 00:05:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { Box, LightMode } from "@chakra-ui/react";
 export default function Home() {
     return (
         <LightMode>
-        <Layout>
+        <Layout className="forced-light-mode-dippy-fix">
             <Box position="relative" maxW="80vw" zIndex={10} mx="auto" className="forced-light-mode-dippy-fix">
                 <HeroSection />
                 <Features />
