7e339baebd6158dd881b7d8267e5a3545429ce4f
Mohzcrea8me <68813250+Mohzcrea8me@users.noreply.github.com>
2022-06-16 15:41:25 -0500

    Update chargeModal.js

--- components/Post/chargeModal.js
+++ components/Post/chargeModal.js
@@ -81,7 +81,7 @@ const ChargeModal = ({ nft, state }) => {
             .charge(
                 {
                     charger_id: nearState.accountId,
-                    post_id: nft.post_id,
+                    post_id: parse_int(nft.post_id),
                     amount:
                         sliderValue().to_string() + "000000000000000000000000",
                 },
