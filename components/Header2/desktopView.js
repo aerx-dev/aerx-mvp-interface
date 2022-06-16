99fef6859f18bf2f9f320b407d02d1436ad5d77f
diposs <onakunle.dipo@gmail.com>
2022-06-16 05:44:57 +0100

    Update desktopView.js

--- components/Header2/desktopView.js
+++ components/Header2/desktopView.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { loginToken, logout } from "../../lib/auth";
 import ChangeLanguage from "./change-language";
 import ConnectWallet from "./connect-wallet";
-import ToggleMode from "./toggle-mode";
 import { nearStore } from "../../stores/near.js";
 
 
