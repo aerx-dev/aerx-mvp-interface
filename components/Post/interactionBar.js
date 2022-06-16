268ade8df3dcc53484848d94e1ac932143acf9df
Mohzcrea8me <68813250+Mohzcrea8me@users.noreply.github.com>
2022-06-16 15:26:03 -0500

    charge function implementation

--- components/Post/interactionBar.js
+++ components/Post/interactionBar.js
@@ -1,14 +1,19 @@
 import { Layout } from "antd";
 import { useState } from "react";
-import { Box , Input, useColorModeValue } from "@chakra-ui/react";
-import { AddIconButton, ChargeOutlineButton, CommentIconButton, ShareIconButton } from "../UI/IconButton";
+import { Box, Input, useColorModeValue } from "@chakra-ui/react";
+import {
+    AddIconButton,
+    ChargeOutlineButton,
+    CommentIconButton,
+    ShareIconButton,
+} from "../UI/IconButton";
 import MemberTag from "./tagmembers";
 import { nearStore } from "../../stores/near";
 import useLongPress from "./useLongPress";
 
 const { Header, Footer, Content } = Layout;
 
-const InteractionBar = ({ nft, onOpen, currentCharge}) => {
+const InteractionBar = ({ nft, onOpen, currentCharge }) => {
     const bdcolorchanger = useColorModeValue("white", "#1B1D1E");
     const nearState = nearStore((state) => state);
     const styles = {
@@ -23,87 +28,80 @@ const InteractionBar = ({ nft, onOpen, currentCharge}) => {
         },
     };
 
-    const [ commentBox , setCommentBox ] = useState(false);
-    const comment = () =>{
-        setCommentBox(!commentBox)
-    }
-    const [longPressCount, setlongPressCount] = useState(0)
+    const [commentBox, setCommentBox] = useState(false);
+    const comment = () => {
+        setCommentBox(!commentBox);
+    };
+    const [longPressCount, setlongPressCount] = useState(0);
 
-  const onLongPress = () => {
-    console.log('longpress is triggered');
-    onOpen();
-  };
+    const onLongPress = () => {
+        console.log("longpress is triggered");
+        onOpen();
+    };
 
-  const onClick = () => {
-    console.log('click is triggered');
-    clickchargePost();
-  }
+    const onClick = () => {
+        console.log("click is triggered");
+        clickchargePost();
+    };
 
-  const defaultOptions = {
-    shouldPreventDefault: true,
-    delay: 500,
-  };
-  const longPressEvent = useLongPress(onLongPress, onClick, defaultOptions);
+    const defaultOptions = {
+        shouldPreventDefault: true,
+        delay: 500,
+    };
+    const longPressEvent = useLongPress(onLongPress, onClick, defaultOptions);
 
-    
     async function clickchargePost() {
-        if (nearState?.aexBalance == 0){
+        if (nearState?.aexBalance == 0) {
             return;
         } else {
-        const amount = 1;
-        nearState.tokenContract
-            .ft_transfer(
-                {
-                    receiver_id: nft.owner_id,
-                    amount: amount.toString(),
-                    memo:
-                        "Charge :zap: from " +
-                        nearState?.accountId +
-                        " for your AEXpost id." +
-                        nft.token_id,
-                },
-                "300000000000000", // attached GAS (optional)
-                1, // attached deposit in yoctoNEAR (optional)
-            )
-            .catch((e) => {
-                console.log("Charge failed!", e);
-                console.log("nft.owner_id", nft.owner_id);
-                toast("error", "Charge failed!", "ChargeIderr");
-            });
+            const amount = 1000000000000000000000000n;
+            nearState.tokenContract
+                .charge(
+                    {
+                        charger_id: nearState.accountId,
+                        post_id: nft.post_id,
+                        amount: amount.toString(),
+                    },
+                    "300000000000000", // attached GAS (optional)
+                )
+                .catch((e) => {
+                    console.log("Charge failed!", e);
+                    console.log("nft.owner_id", nft.owner_id);
+                    toast("error", "Charge failed!", "ChargeIderr");
+                });
         }
     }
 
     return (
         <>
-            <Footer style={styles.footer} className="flex align-middle gap-2" >
-                <ChargeOutlineButton {...longPressEvent}/>{currentCharge}
-                <CommentIconButton onClick={comment}/>0
-                <ShareIconButton opacity={0.7} ml={2}/>0
-                <MemberTag style={styles.tag}/>
+            <Footer style={styles.footer} className="flex align-middle gap-2">
+                <ChargeOutlineButton {...longPressEvent} />
+                {currentCharge}
+                <CommentIconButton onClick={comment} />0
+                <ShareIconButton opacity={0.7} ml={2} />0
+                <MemberTag style={styles.tag} />
             </Footer>
             <Footer>
-                { commentBox ? 
-                    <Box 
-                        flexDirection="row"
-                        display="flex"
-                        alignItems="center">
-                            <Input
-                                maxLength={500}
-                                type="text"
-                                data-path="text"
-                                placeholder="comment"
-                                borderRadius={20}
-                                size="sm"
-                                border="none"
-                                bg={bdcolorchanger}
-                            />
+                {commentBox ? (
+                    <Box flexDirection="row" display="flex" alignItems="center">
+                        <Input
+                            maxLength={500}
+                            type="text"
+                            data-path="text"
+                            placeholder="comment"
+                            borderRadius={20}
+                            size="sm"
+                            border="none"
+                            bg={bdcolorchanger}
+                        />
                         <Box>
-                            <AddIconButton/>
+                            <AddIconButton />
                         </Box>
-                    </Box> : null}
+                    </Box>
+                ) : null}
             </Footer>
         </>
     );
-}
+};
 
 export default InteractionBar;
