62a262c2f7544a3959bf52855116c26c1eeee21a
diposs <onakunle.dipo@gmail.com>
2022-06-17 04:49:57 +0100

    Update postHeader.js

--- components/Post/postHeader.js
+++ components/Post/postHeader.js
@@ -18,7 +18,7 @@ const PostHeader = ({metadata, isUserMsg, currentProfile, nft}) => {
         }
     };
     const pft ={currentProfile};
-    console.log('hbh',pft);
+    console.log('hbh',pft.currentProfile.metadata.media);
     
     const nearState = nearStore((state) => state);
 
