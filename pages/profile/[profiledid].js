bcf2ee0b924dbcaa9d2267bdfbe2047a11860165
diposs <onakunle.dipo@gmail.com>
2022-06-18 17:53:48 +0100

    Update [profiledid].js

--- pages/profile/[profiledid].js
+++ pages/profile/[profiledid].js
@@ -2,12 +2,21 @@ import { nearStore } from "../../stores/near.js";
 import { useState } from "react";
 import Profile from "../../components/ProfileMain";
 import { Box } from "@chakra-ui/react";
+
+export async function getServerSideProps({ params }) {
+  const profiledid = params.profiledid;
+  return {
+    props: {
+      profiledid,
+    }
+  }
+}
+
 const ProfilePage = () => {
     return (
         <Box>
-            <Profile />
+            <Profile profiledid = {profiledid}  />
         </Box>
     );
 };
-
 export default ProfilePage;
