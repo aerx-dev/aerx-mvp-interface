5cef508f06b946d489124b4d3cec76983dc9ca1e
diposs <onakunle.dipo@gmail.com>
2022-06-18 02:32:24 +0100

    Update dd.js

--- pages/profile/dd.js
+++ pages/profile/dd.js
@@ -1,1 +1,12 @@
-dd
+import Profile from "../../components/Profile";
+import { Box } from "@chakra-ui/react";
+
+const ProfilePage = () => {
+    return (
+        <Box>
+            <Profile />
+        </Box>
+    );
+};
+
+export default ProfilePage;
