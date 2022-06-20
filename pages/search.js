884cb1f0006828f9dfc00023cc1bbc07911ccb35
diposs <onakunle.dipo@gmail.com>
2022-06-20 18:34:47 +0100

    Create search.js

---
+++ pages/search.js
@@ -0,0 +1,12 @@
+import Search from "../components/Search";
+import { Box } from "@chakra-ui/react";
+
+const SearchPage = () => {
+    return (
+        <Box>
+            <Search />
+        </Box>
+    );
+};
+
+export default SearchPage;
