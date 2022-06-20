994e39c3977c2553ba2254138a7077a9efa9a106
diposs <onakunle.dipo@gmail.com>
2022-06-20 18:39:57 +0100

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,12 @@
+import { useRouter } from 'next/router'
 import Search from "../components/Search";
 import { Box } from "@chakra-ui/react";
 
 const SearchPage = () => {
+    const { query } = useRouter();
     return (
         <Box>
-            <Search />
+            <Search searched={query.id} />
         </Box>
     );
 };
