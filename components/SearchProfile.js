9fbd9818936e0e2ad3cad73a3305bea910aef905
diposs <onakunle.dipo@gmail.com>
2022-06-21 01:33:16 +0100

    Update SearchProfile.js

--- components/SearchProfile.js
+++ components/SearchProfile.js
@@ -1,5 +1,4 @@
 import {
-    Avatar,
     Box,
     Input,
     useColorMode,
@@ -7,34 +6,20 @@ import {
 } from "@chakra-ui/react";
 import useCustomToast from "../hooks/useCustomToast";
 import {
-    AddAudioIcon,
-    AddIconButton,
-    SearchIconButton,
-    AddImageIcon,
-    RepeatIconButton,
-} from "./UI/IconButton";
+    SearchIconButton } from "./UI/IconButton";
 import usePinata from "../hooks/usePinata";
 import { useState, useEffect, useRef } from "react";
 import { nearStore } from "../stores/near";
-import { getBalance } from "../lib/tokenContract";
+import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
-import useFetchPosts from "../hooks/useFetchPosts";
-import { supabase, postToSupa } from "../lib/supabaseClient";
 
 function SearchProfile({ bg }) {
     const nearState = nearStore((state) => state);
     // const [balance, setBalance] = useState(0);
-    const refresh = useFetchPosts();
     const toast = useCustomToast();
-
-    // The uploaded image which will be deployed through IPFS
-    const [uploadFile, setUploadFile] = useState();
-    // Ipsf hook with details and upload hook.
-    const ipfsData = usePinata(uploadFile, toast);
-
-    const [body, setBody] = useState({
+    const router = useRouter();
+    const [sbody, setSbody] = useState({
         text: "",
-        media_type: "text",
     });
 
     const { t } = useTranslation("profile");
@@ -42,112 +27,28 @@ function SearchProfile({ bg }) {
     const filter = colorMode === "light" ? "invert(0)" : "invert(0)";
     const fill = useColorModeValue("gray", "white");
 
-    useEffect(() => {
-        (async () => {
-            if (nearState.tokenContract) {
-                let { formatted } = await getBalance(nearState);
-                toast(
-                    "info",
-                    "Your balance is " + formatted + " AEX$",
-                    "BalanceId",
-                );
-            }
-        })(); // IIFE
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
-    async function createPost() {
-        if (!body.text) {
-            toast("warning", "Post cannot be empty!", "feedpage");
-            return;
-        }
 
-        let postToSave = {
-            title: "AERX ContentNFT for " + nearState.accountId,
-            description: body.text,
-            media: ipfsData.fileUrl,
-            media_hash: ipfsData.urlSha256,
-            issued_at: new Date().toISOString(),
-            extra: JSON.stringify(body),
+    async function searchenter() {
+        try{
+            router.push({
+                    pathname: "/search",
+                    query: {
+                      id: sbody.text
+                    }
+                  });
         };
-        console.log(body);
-        console.log("Post to save: ", postToSave);
-        try {
-            const minted_post = await nearState.pnftContract.mint_post(
-                {
-                    user_id: nearState.accountId,
-                    token_metadata: postToSave,
-                },
-                "300000000000000", // attached GAS
-            );
-            console.log(post);
-            toast(
-                "success",
-                "AERX ContentNFT with id : " +
-                    minted_post.token_id +
-                    "was minted successfully!",
-                "CNFTsccss",
-            );
-            postToSave.tokenId = minted_post.token_id;
-            postToSave.ownerId = minted_post.owner_id;
-            postToSave.postId = minted_post.token_id;
-            postToSupa(postToSave, toast);
-        } catch (e) {
-            console.log("NFT could not be minted! Error: " + e.message);
-            toast(
-                "error",
-                "NFT could not be minted! Error: " + e.message,
-                "CNFTerror",
-            );
-        }
+
     }
 
     // Reffs to the content data
-    const inputAudio = useRef(null);
-    const onAudioClick = () => {
-        inputAudio.current.click();
-        setBody((prevBody) => {
-            return {
-                ...prevBody,
-                type: "audio",
-            };
-        });
-    };
-
-    const inputImg = useRef(null);
-    const onImgClick = () => {
-        inputImg.current.click();
-        setBody((prevBody) => {
-            return {
-                ...prevBody,
-                type: "image",
-            };
-        });
-    };
-    function fileChange(event) {
-        const { files } = event.target;
-        if (files) {
-            // // TODO check what type it is
-            const filename = files[0].name;
-            var parts = filename.split(".");
-            const fileType = parts[parts.length - 1];
-            setBody((prevBody) => {
-                return {
-                    ...prevBody,
-                    media_extension: fileType,
-                };
-            });
-            console.log(body);
-            setUploadFile(() => event.target.files[0]);
-        }
-    }
-
+   
     function update(e) {
         const path = e.currentTarget.dataset.path;
         const val = e.currentTarget.value;
-        setBody((prevBody) => {
+        setSbody((prevSbody) => {
             return {
-                ...prevBody,
+                ...prevSbody,
                 [path]: val,
             };
         });
@@ -185,10 +86,7 @@ function SearchProfile({ bg }) {
                 bg={useColorModeValue("white", "#1B1D1E")}
             />
 
-            <Box display="none">
-                <input ref={inputImg} />
-            </Box>
-            <Box ml={3}>
+            <Box onClick={searchenter} ml={3}>
                 <SearchIconButton />
             </Box>
         </Box>
