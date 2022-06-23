933cd6a90fc6aea24f4b2a18c3a018fb0908d811
diposs <onakunle.dipo@gmail.com>
2022-06-23 21:14:04 +0100

    Update interactionBar.js

--- components/Post/interactionBar.js
+++ components/Post/interactionBar.js
@@ -149,9 +149,9 @@ const InteractionBar = ({ nft, onOpen, currentCharge, currentComment }) => {
                 null ) : (
             <>
             <Footer style={styles.footer} className="flex align-middle gap-2">
-         {nft?.owner_id == nearState?.accountId  ? ( {parseFloat({currentCharge}) == 0 || parseFloat({currentCharge}) == null ? (
+         {nft?.owner_id == nearState?.accountId  ? ( {parseFloat({currentCharge}) == 0 ? (
                 <ChargeOutlineButton /> ):( <ChargeButton />)}) : 
-        ( {parseFloat({currentCharge}) == 0 || parseFloat({currentCharge}) == null ? ( <ChargeOutlineButton {...longPressEvent} />):(<ChargeOutlineButton {...longPressEvent} />)})}
+        ( {parseFloat({currentCharge}) == 0 ? ( <ChargeOutlineButton {...longPressEvent} />):(<ChargeOutlineButton {...longPressEvent} />)})}
                 {currentCharge}
                 <CommentIconButton onClick={comment} />{currentComment}
                 <ShareIconButton opacity={0.7} ml={2} />0
