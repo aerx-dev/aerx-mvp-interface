0af2408b8b3c3bbe0e15af2f9e76578d813bf70e
diposs <onakunle.dipo@gmail.com>
2022-06-24 18:26:19 +0100

    Update tagmembers.js

--- components/Post/tagmembers.js
+++ components/Post/tagmembers.js
@@ -5,18 +5,21 @@ const MemberTag = ({...rest}) => {
     return (
         <Box {...rest}>
             <TagAddButton/>
-            <Tag mt={1} borderRadius="full" >
+            <Tag mt={1} bg='transparent' borderRadius="full" >
                 <Avatar
+                bg='transparent'
                 name="Tag member 1"
                 src='/images/Hello.png'
                 size="2xs"
                 />
                 <Avatar
+                bg='transparent'
                 name="Tag member 1"
                 src='/images/Laugh.png'
                 size="2xs"
                 /> 
                 <Avatar
+                bg='transparent'
                 name="Tag member 1"
                 src='/images/To-tears.png'
                 size="2xs"
