80911c234bfb4bf37615bcc9a5a476c2a1a6dabe
masafumimori <m.masafumi@outlook.com>
2022-07-13 11:19:45 +0900

    add window global type

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,14 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// import type { IPFS } from "ipfs-core-types";
+
+export {};
+
+declare global {
+    interface Window {
+        accountId: any;
+        ipfs: any; // TODO: SHOULD BE ABLE TO USE IPFS TYPE IMPORTED
+    }
+}
