374661d3bf7bdaa405a7b4323336b531c7a3b3b6
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-07-14 23:24:47 +0800

    change profiledid to string

--- components/ProfileMain/SideBar/index.tsx
+++ components/ProfileMain/SideBar/index.tsx
@@ -29,7 +29,7 @@ export default function SideBar({
 
             var res = await nearState.pnftContract.profile_by_id({
                 user_id: nearState.accountId,
-                user_to_find_id: profiledlooker.profiledid,
+                user_to_find_id: profiledlooker.profiledid.to_string(),
             });
 
             setCurrentProfileview(res);
