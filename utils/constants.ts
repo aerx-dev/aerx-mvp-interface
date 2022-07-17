0cb6a3d611f39c1affefe9215b4ae47cd6e42ec1
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-07-17 20:25:52 +0800

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,4 +3,4 @@ export const TOKEN_CONTRACT_NAME = "tokentest.mohzcrea8me.testnet"; //to load ae
 export const PROFILE_CONTRACT_NAME = "aprofiletest.mohzcrea8me.testnet"; //to load profile_contract contract
 export const DEX_CONTRACT_NAME = "swaptest.mohzcrea8me.testnet"; //to load profile_contract contract
 
-export const EXCHANGE_CURRENCY = ["NEAR", "AERX"] as const;
+export const EXCHANGE_CURRENCY = ["NEAR", "AEX"] as const;
