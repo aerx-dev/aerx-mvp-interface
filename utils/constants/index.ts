36a1a6fe2f8b056854faa31c72dfccc360778074
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-08-08 22:51:56 +0800

    Update index.ts

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,6 +1,6 @@
 //this is just for testing purpose will be moved to env
-export const TOKEN_CONTRACT_NAME = "tokentest.mohzcrea8me.testnet"; //to load aex_token contract
-export const PROFILE_CONTRACT_NAME = "aprofiletest.mohzcrea8me.testnet"; //to load profile_contract contract
-export const DEX_CONTRACT_NAME = "swaptest.mohzcrea8me.testnet"; //to load profile_contract contract
+export const TOKEN_CONTRACT_NAME = "aextest.mohzcrea8me.testnet"; //to load aex_token contract
+export const PROFILE_CONTRACT_NAME = "aerxtest.mohzcrea8me.testnet"; //to load profile_contract contract
+export const DEX_CONTRACT_NAME = "aeswaptest.mohzcrea8me.testnet"; //to load profile_contract contract
 
 export const EXCHANGE_CURRENCY = ["NEAR", "AEX"] as const;
